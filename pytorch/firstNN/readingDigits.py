85388d5ec1a41cc6da100d1cf3a5f0ec8203efed
MatthewLee311 <53510176+MatthewLee311@users.noreply.github.com>
2021-06-01 08:58:35 -0700

    Add files via upload

--- pytorch/firstNN/readingDigits.py
+++ pytorch/firstNN/readingDigits.py
@@ -47,3 +47,32 @@ for q in range(heightLoops):
 
 cv2.imshow("Press any key to exit", img)
 cv2.waitKey()
+
+framesizeReceipt = 50
+widthLoops = int(printedImgW/framesizeReceipt * 2)
+heightLoops = int(printedImgH/framesizeReceipt * 2)
+
+for q in range(heightLoops):
+    for i in range(widthLoops):
+        # setting up frames
+        topLeftX = int(framesizeReceipt/2 * i)
+        topLeftY = int(framesizeReceipt/2 * q)
+        bottomLeftX = int(framesizeReceipt/2 * (i+1))
+        bottomRightY = int(framesizeReceipt/2 * (q+1))
+        cv2.rectangle(printedImg,(topLeftX, topLeftY), (bottomLeftX, bottomRightY), (255, 0, 0), 1)
+        roi = printedImg[topLeftY:bottomRightY, topLeftX:bottomLeftX]
+
+        roi = cv2.resize(roi, (28, 28), interpolation = cv2.INTER_AREA)
+        tmp = np.zeros((1, roi.shape[2], roi.shape[0], roi.shape[0]), dtype=float)
+        for i, row in enumerate(roi):
+            for j, col in enumerate(row):
+                for k, entry in enumerate(col):
+                    tmp[0][k][i][j] = entry/255.0
+        roi = roi.reshape(-1, 28, 28, 1)
+        roiTensor = torch.Tensor(tmp)
+        results = digitsModel.predict(roi)
+        print(results)
+
+
+cv2.imshow("Press any key to exit", printedImg)
+cv2.waitKey()
